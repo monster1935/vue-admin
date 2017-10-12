77e42f6781d27435c75695b18d80828cd15b3003
monster1935 <946815480@qq.com>
2017-10-12 12:52:03 +0800

    修改el-tabs的位置

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,23 @@
         <app-nav></app-nav>
       </div>
       <div class="app-wrap">
+        <!-- 此处放置el-tabs代码 -->
+        <div class="template-tabs">
+          <el-tabs
+            v-model="activeIndex"
+            type="card"
+            closable
+            @tab-click="tabClick"
+            v-if="options.length"
+            @tab-remove="tabRemove">
+            <el-tab-pane
+              :key="item.name"
+              v-for="(item, index) in options"
+              :label="item.name"
+              :name="item.route">
+            </el-tab-pane>
+          </el-tabs>
+        </div>
         <router-view/>
       </div>
     </div>
@@ -21,6 +38,55 @@ export default {
   name: 'app',
   components: {
     AppNav
+  },
+  methods: {
+    tabClick () {
+      this.$router.push({path: this.activeIndex});
+    },
+    tabRemove (targetName) {
+      if(targetName == '/') {
+        return;
+      }
+      this.$store.commit('delete_tabs', targetName);
+      if (this.activeIndex === targetName) {
+        // 设置当前激活的路由
+        if (this.options && this.options.length >= 1) {
+          this.$store.commit('set_active_index', this.options[this.options.length-1].route);
+          this.$router.push({path: this.activeIndex});
+        } else {
+          this.$router.push({path: '/'});
+        }
+      }
+    }
+  },
+  computed: {
+    options () {
+      return this.$store.state.options;
+    },
+    activeIndex: {
+      get () {
+        return this.$store.state.activeIndex;
+      },
+      set (val) {
+        this.$store.commit('set_active_index', val);
+      }
+    }
+  },
+  watch: {
+    '$route'(to) {
+      let flag = false;
+      for (let option of this.options ) {
+        if (option.name === to.name) {
+          flag = true;
+          this.$store.commit('set_active_index', to.path);
+          break
+        }
+      }
+      if (!flag) {
+        this.$store.commit('add_tabs', {route: to.path, name: to.name});
+        this.$store.commit('set_active_index', to.path);
+      }
+    }
   }
 }
 </script>
--- src/components/Template.vue
+++ src/components/Template.vue
@@ -1,22 +1,6 @@
 <!-- 模板组件，用于模拟不同路由下的组件显示 -->
 <template>
   <div class="router-template">
-    <div class="template-tabs">
-      <el-tabs
-        v-model="activeIndex"
-        type="card"
-        closable
-        @tab-click="tabClick"
-        v-if="options.length"
-        @tab-remove="tabRemove">
-        <el-tab-pane
-          :key="item.name"
-          v-for="(item, index) in options"
-          :label="item.name"
-          :name="item.route">
-        </el-tab-pane>
-      </el-tabs>
-    </div>
     <div class="content-wrap">
       {{$route.name}}
     </div>
@@ -26,59 +10,9 @@
 <script>
   export default {
     data () {
-      return {
-
-      };
-    },
-    methods: {
-      tabClick () {
-        this.$router.push({path: this.activeIndex});
-      },
-      tabRemove (targetName) {
-        if(targetName == '/') {
-          return;
-        }
-        this.$store.commit('delete_tabs', targetName);
-        if (this.activeIndex === targetName) {
-          // 设置当前激活的路由
-          if (this.options && this.options.length >= 1) {
-            this.$store.commit('set_active_index', this.options[this.options.length-1].route);
-            this.$router.push({path: this.activeIndex});
-          } else {
-            this.$router.push({path: '/'});
-          }
-        }
-      }
-    },
-    computed: {
-      options () {
-        return this.$store.state.options;
-      },
-      activeIndex: {
-        get () {
-          return this.$store.state.activeIndex;
-        },
-        set (val) {
-          this.$store.commit('set_active_index', val);
-        }
-      }
-    },
-    watch: {
-      '$route'(to) {
-        let flag = false;
-        for (let option of this.options ) {
-          if (option.name === to.name) {
-            flag = true;
-            this.$store.commit('set_active_index', to.path);
-            break
-          }
-        }
-        if (!flag) {
-          this.$store.commit('add_tabs', {route: to.path, name: to.name});
-          this.$store.commit('set_active_index', to.path);
-        }
-      }
+      return {};
     }
+
   }
 </script>
 <style>
